536ff32557c724674e80eb86f393052e9007fa93
Boobalan D <boobalandhanaraj@gmail.com>
2024-01-22 11:30:23 +0530

    logout option in home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { HomeService } from '../../services/home.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +12,21 @@ import { HomeService } from '../../services/home.service';
 export class HomeComponent {
   productList: any;
 
-  constructor(private api: HomeService) {}
+  constructor(
+    private api: HomeService,
+    private authService: AuthService,
+    private route: Router,
+    private toast: NgToastService
+  ) {}
 
   ngOnInit(): void {
     this.api.getProductList().subscribe((res) => {
       this.productList = res.result;
     });
+  }
+
+  onAdminLogout() {
+    this.authService.setAuthenticationStatus(false);
+    this.route.navigate(['/login']);
   }
 }
