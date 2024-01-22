ca28e035cd1dd59a859598f4129d8932d56ae640
Boobalan D <boobalandhanaraj@gmail.com>
2024-01-22 16:34:33 +0530

    Navbar UI design and its functionalities

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  productList: any;
+  adminDetails: any;
 
+  constructor(
+    private authService: AuthService,
+    private route: Router,
+    private toast: NgToastService,
+    private data: DataService
+  ) {}
+
+  ngOnInit(): void {
+    this.data.loginResponse$.subscribe((response) => {
+      this.adminDetails = response;
+    });
+  }
+
+  onAdminLogout() {
+    this.authService.setAuthenticationStatus(false);
+    this.route.navigate(['/login']);
+    this.toast.success({
+      detail: 'Sucessfully Logged out!',
+      summary: 'Enter the credentials to Login again',
+      duration: 5000,
+      position: 'topRight',
+    });
+  }
 }
