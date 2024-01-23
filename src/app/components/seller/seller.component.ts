461eb83ce063f4febd2e482eef37ed1dd2ef968c
Boobalan D <boobalandhanaraj@gmail.com>
2024-01-23 15:04:23 +0530

    added method for to post new seller

--- src/app/components/seller/seller.component.ts
+++ src/app/components/seller/seller.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SellerService } from '../../services/seller.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-seller',
@@ -7,11 +8,35 @@ import { SellerService } from '../../services/seller.service';
   styleUrl: './seller.component.css',
 })
 export class SellerComponent {
+  formValue!: FormGroup;
   sellerDetails: any;
 
-  constructor(private api: SellerService) {}
+  constructor(private api: SellerService, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.InitiatePage();
+    this.formValue = this.formBuilder.group({
+      Name: [''],
+      Email: [''],
+      PhoneNumber: [''],
+    });
+  }
+
+  onAddNewSeller() {
+    const sellerData = {
+      SellerName: this.formValue.value.Name,
+      Email: this.formValue.value.Email,
+      PhoneNumber: this.formValue.value.PhoneNumber,
+    };
+
+    this.api.postNewSeller(sellerData).subscribe((res) => {
+      console.log(res);
+      this.InitiatePage();
+      this.formValue.reset();
+    });
+  }
+
+  private InitiatePage() {
     this.api.getSellersList().subscribe((res) => {
       this.sellerDetails = res;
       console.log(this.sellerDetails);
--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +14,15 @@ export class SellerService {
     const url = `${this.BaseUrl}/seller/GetSeller`;
 
     return this.http.get<any>(url);
+  }
+
+  postNewSeller(data: any): Observable<any> {
+    const url = `${this.BaseUrl}/seller/AddSeller`;
+
+    return this.http.post(url, data).pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
   }
 }
