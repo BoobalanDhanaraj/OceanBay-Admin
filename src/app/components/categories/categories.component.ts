8a36c892f71f12b9244feb319e2511d2a5b4c04a
Boobalan D <boobalandhanaraj@gmail.com>
2024-01-29 18:16:24 +0530

    method to get all the categories

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.css'
+  styleUrl: './categories.component.css',
 })
 export class CategoriesComponent {
+  allCategories: any;
 
+  constructor(private api: CategoriesService) {}
+
+  ngOnInit(): void {
+    this.onCategoriesList();
+  }
+
+  private onCategoriesList() {
+    this.api.getAllCategoriesList().subscribe((res) => {
+      this.allCategories = res.result;
+      console.log(res.result);
+    });
+  }
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoriesService {
+  private BaseUrl = 'https://localhost:7122/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllCategoriesList(): Observable<any> {
+    const url = `${this.BaseUrl}/categories/AllCategories`;
+
+    return this.http.get<any>(url);
+  }
 }
