483b88667863900d276217f03298b7f88bc942d5
Boobalan D <boobalandhanaraj@gmail.com>
2024-01-31 16:45:03 +0530

    category list in dropdown

--- src/app/components/products/add-products/add-products.component.ts
+++ src/app/components/products/add-products/add-products.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SharedCategoriesService } from '../../../helpers/shared-categories.service';
 import { CategoriesService } from '../../../services/categories.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-products',
@@ -9,13 +10,25 @@ import { CategoriesService } from '../../../services/categories.service';
 })
 export class AddProductsComponent {
   categories: any;
+  formValue!: FormGroup;
 
   constructor(
     private categoryApi: CategoriesService,
-    private sharedCategoryApi: SharedCategoriesService
+    private sharedCategoryApi: SharedCategoriesService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
+    this.formValue = this.formBuilder.group({
+      name: [''],
+      productImageUrl: [''],
+      price: [''],
+      stockQuantity: [''],
+      categoryId: [''],
+      subcategoryId: [''],
+      sellerId: [''],
+    });
+
     this.categoryApi.getAllCategoriesList().subscribe((res) => {
       this.categories = res.result;
       console.log(this.categories);
