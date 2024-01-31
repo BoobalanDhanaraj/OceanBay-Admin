99f45ee76aa2f75939a2eba1ecbb1fdb16964e8b
Boobalan D <boobalandhanaraj@gmail.com>
2024-01-31 15:15:14 +0530

    added method to get the list of categories for adding product

--- src/app/components/products/add-products/add-products.component.ts
+++ src/app/components/products/add-products/add-products.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { SharedCategoriesService } from '../../../helpers/shared-categories.service';
+import { CategoriesService } from '../../../services/categories.service';
 
 @Component({
   selector: 'app-add-products',
   templateUrl: './add-products.component.html',
-  styleUrl: './add-products.component.css'
+  styleUrl: './add-products.component.css',
 })
 export class AddProductsComponent {
+  categories: any;
 
+  constructor(
+    private categoryApi: CategoriesService,
+    private sharedCategoryApi: SharedCategoriesService
+  ) {}
+
+  ngOnInit(): void {
+    this.categoryApi.getAllCategoriesList().subscribe((res) => {
+      this.categories = res.result;
+      console.log(this.categories);
+    });
+
+    this.sharedCategoryApi.categoryList$.subscribe((response) => {
+      this.categories = response;
+    });
+  }
 }
